3e6b0fb580078ed91a664c974cec3786dcfa69f0
haolil <lin_rong2005@hotmail.com>
2023-03-04 03:11:49 -0500

    done

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+/**
+ * File Name: db.js
+ * Author: Shiaw-Rong Lin
+ * Student ID: 301293061
+ * Web App Name: My Favorite Books
+ */
+
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://lin86717377:Mt3NEoVrqiJbdQEJ@cluster0.la0j9qt.mongodb.net/?retryWrites=true&w=majority"
+};
